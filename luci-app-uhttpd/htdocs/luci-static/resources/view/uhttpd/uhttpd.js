41e8fb849895b48aa01ea1f2d96acf5122153ffc
Ayushman <ayushmantripathi7724@gmail.com>
2023-04-07 22:39:32 +0530

    add tabs

--- luci-app-uhttpd/htdocs/luci-static/resources/view/uhttpd/uhttpd.js
+++ luci-app-uhttpd/htdocs/luci-static/resources/view/uhttpd/uhttpd.js
@@ -16,7 +16,7 @@ return view.extend({
       m = new form.Map('uhttpd', _('uHTTPd'),
       _('A lightweight single-threaded HTTP(S) server'));
     
-      ucs = m.section(form.TypedSection, 'uhttpd');
+      ucs = m.section(form.Typedsection, 'uhttpd');
       ucs.addremove = true;
       ucs.anonymous = false;
 
@@ -49,7 +49,50 @@ return view.extend({
         if (!(have_http_listener || have_https_listener)) {
           return [null, "must listen on at least one address:port"];
         }
-        return DynamicList.validate(value, section_id);
+        return form.DynamicList.validate(section_id, value);
+      }
+
+      
+      lhttps = ucs.taboption("general", form.DynamicList, "listen_https", _("HTTPS listener (address:port)"), _("Bind to specific interface:port (by specifying interface address"))
+      lhttps.datatype = "list(ipaddrport(1))"
+      lhttps.depends("cert")
+      lhttps.depends("key")
+
+      lhttps.validate=function(section_id, value) {
+        let have_https_listener = false;
+        let have_http_listener = false;
+      
+        if (lhttps && lhttps.formvalue(section_id) && (lhttps.formvalue(section_id).length > 0)) {
+          for (let i = 0; i < lhttps.formvalue(section_id).length; i++) {
+            let v = lhttps.formvalue(section_id)[i];
+            if (v && (v !== "")) {
+              have_https_listener = true;
+              break;
+            }
+          }
+          if (have_https_listener && ((!cert_file) || (!cert_file.formvalue(section_id)) || (cert_file.formvalue(section_id) === ""))) {
+            return [null, "must have certificate when using https"];
+          }
+          if (have_https_listener && ((!key_file) || (!key_file.formvalue(section_id)) || (key_file.formvalue(section_id) === ""))) {
+            return [null, "must have key when using https"];
+          }
+        }
+      
+        if (lhttp && lhttp.formvalue(section_id) && (lhttp.formvalue(section_id).length > 0)) {
+          for (let i = 0; i < lhttp.formvalue(section_id).length; i++) {
+            let v = lhttp.formvalue(section_id)[i];
+            if (v && (v !== "")) {
+              have_http_listener = true;
+              break;
+            }
+          }
+        }
+      
+        if (!(have_http_listener || have_https_listener)) {
+          return [null, "must listen on at least one address:port"];
+        }
+      
+        return DynamicList.validate(section_id, value);
       }
 
 
